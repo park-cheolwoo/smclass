4dc67593db4ed00a7a98fe935577275473e6055b
kosmo <bd8860@naver.com>
2024-12-16 09:39:56 +0900

    12월 16일 커밋

--- ag01_merge/coupon/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from coupon.models import Coupon
-
-@admin.register(Coupon)
-class CouponAdmin(admin.ModelAdmin):
-  list_display = ['attendance','discount','used_from','used_to']
--- ag01_merge/member/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from member.models import Member
-
-@admin.register(Member)
-class MemberAdmin(admin.ModelAdmin):
-  list_display = ['id', 'name', 'nickname', 'mDate']
